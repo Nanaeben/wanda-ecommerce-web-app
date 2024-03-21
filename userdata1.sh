d5155735cf063ca53a30bf29609ff7ddbf93a768
Gwei Michael Wawa <101484680+Michaelgwei86@users.noreply.github.com>
2024-03-21 14:37:12 +0100

    Update userdata1.sh

--- userdata1.sh
+++ userdata1.sh
@@ -11,13 +11,13 @@ sudo yum install git -y
 
 
 # Clone the repository
-git clone https://github.com/cvamsikrishna11/ecommerce-web-app.git
+git clone https://github.com/Michaelgwei86/wanda-ecommerce-web-app.git
 
 # Copy the files inside the cloned folder to the desired location
-sudo cp -r ecommerce-web-app/server1/* /var/www/html/
+sudo cp -r wanda-ecommerce-web-app/server1/* /var/www/html/
 
 # To get the current hostname address
-echo "<center><p>Running the website from instance host: $(hostname -f)</p></center>" >> /var/www/html/index.html
+# echo "<center><p>Running the website from instance host: $(hostname -f)</p></center>" >> /var/www/html/index.html
 
 # Start and enable the HTTP server
 sudo systemctl start httpd
